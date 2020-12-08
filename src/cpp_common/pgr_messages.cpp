1d26ef89a76725e6c87d8a8aac4aaa525dc0455a
Vicky Vergara <vicky@georepublic.de>
2020-12-08 08:21:35 -0600

    Fix for Issue 1770 on 3.1 (#1771)
    * [CI] adding action for clang compiler
    * [C] Ingnoring postgres warnings
    * [dijkstra] compiling with clang
    * [common] compiling with clang
    * [astar] compiles with clang
    * [ksp] compiles with clang
    * [tsp] compiles with clang
    * [alpha_shape] compiles with clang
    * [tspEuclidean] compiles with clang
    * [witPoints] compiles with clang
    * [bellman_ford] compiles with clang
    * [chinese] compiles with clang
    * [transitiveClosure] compiles with clang
    * [breadthFirstSearch] compiles with clang
    * [components] compiles with clang
    * [maxflow] pgr_maxflow.hpp compiles with clang
    * [maxflow] compiles with clang
    * [lineGraph] compiles with clang
    * [bdAstar] compiles with clang
    * [bdDijkstra] compiles with clang
    * [trsp] compiles with clang
    * [pickDeliver] compiles with clang
    * [dijkstra][docqueries] giving a new correct alternate result
    * Removing a test of a function that does not exist on pgRouting
    * removing internal query tests that were created when developing contraction and are not used
    * [tsp][docqueries] Modification of rand() gives new results
    * roll back changes that affect ksp
    * [C++] moving pgr_messages.cpp to cpp_common
    * roll back changes that affect trsp
    * src/common/basePath_SSEC.cpp compiles with clang
    * src/trsp/pgr_trspHandler.cpp compiles with clang
    * [ci][pgtap] adding testing after clang compilation
    * [doc] adding NEWS & Release notes
    Also updating compilation_time and compiler & hash on pgr_full_version
    * [build] rolling back build-extension-update-files1.pl
    * [C++] adding some minimal changes from the comments of the PR

--- src/cpp_common/pgr_messages.cpp
+++ src/cpp_common/pgr_messages.cpp
@@ -1,8 +1,8 @@
 /*PGR-GNU*****************************************************************
 
-FILE: pd_problem.h
+FILE: pgr_pickDeliver.h
 
-Copyright (c) 2017 pgRouting developers
+Copyright (c) 2015 pgRouting developers
 Mail: project@pgrouting.org
 
 ------
@@ -23,37 +23,61 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
  ********************************************************************PGR-GNU*/
 
-/*! @file */
 
-#ifndef INCLUDE_VRP_PD_PROBLEM_H_
-#define INCLUDE_VRP_PD_PROBLEM_H_
-#pragma once
-
+#include <string>
+#include <sstream>
 #include "cpp_common/pgr_messages.h"
-#include "vrp/initials_code.h"
-
-
 
 namespace pgrouting {
-namespace vrp {
 
-class Pgr_pickDeliver;
 
-class PD_problem {
- public:
-    explicit PD_problem(Pgr_pickDeliver* p_problem);
-    PD_problem(const PD_problem &problem);
-    PD_problem() { }
 
- public:
-     static Pgr_messages msg;
+std::string
+Pgr_messages::get_log() const {
+    auto str = log.str();
+    return str;
+}
 
- protected:
-     static Pgr_pickDeliver* problem;
-     PD_problem& operator=(const PD_problem &) {return *this;};
-};
+std::string
+Pgr_messages::get_notice() const {
+    auto str = notice.str();
+    return str;
+}
 
-}  //  namespace vrp
+bool
+Pgr_messages::has_error() const {
+    return  !error.str().empty();
+}
+
+std::string
+Pgr_messages::get_error() const {
+    auto str = error.str();
+    return str;
+}
+
+#ifndef NDEBUG
+std::string
+Pgr_messages::get_dbglog() const {
+    return dbg_log.str();
+}
+#endif
+
+void
+Pgr_messages::clear() {
+    log.str("");
+    log.clear();
+
+    notice.str("");
+    notice.clear();
+
+    error.str("");
+    error.clear();
+
+#ifndef NDEBUG
+    dbg_log.str("");
+    dbg_log.clear();
+#endif
+}
+
+
 }  //  namespace pgrouting
-
-#endif  // INCLUDE_VRP_PD_PROBLEM_H_
