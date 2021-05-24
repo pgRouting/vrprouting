81b8b4743ea900d138268d85a5c0ebeb0412f9c4
cvvergara <vicky@georepublic.de>
2021-05-24 10:37:07 -0500

    [docqueries] Adding pseudo-tests to check data

---
+++ docqueries/dataCheck/data_check.test.sql
@@ -0,0 +1,10 @@
+SET search_path TO 'example2', 'public';
+
+SELECT count(*) FROM example2.shipments;
+SELECT count(*) FROM example2.physical_vehicles;
+SELECT count(*) FROM example2.vehicles;
+SELECT count(*) FROM example2.depot;
+SELECT count(*) FROM example2.locations;
+SELECT count(*) FROM example2.timeMatrix;
+SELECT count(*) FROM example2.timeMatrix WHERE assigned;
+SELECT count(*) FROM example2.timeMatrix WHERE NOT assigned;
