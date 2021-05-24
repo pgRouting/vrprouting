82c9e9cf95001018c2a01523106ae59aeb5cc931
cvvergara <vicky@georepublic.de>
2021-05-24 10:37:13 -0500

    [pickDeliver] connecting code & driver

---
+++ src/pickDeliver/pickDeliver.c
@@ -0,0 +1,450 @@
+/*PGR-GNU*****************************************************************
+File: pickDeliver.c
+
+Copyright (c) 2021 pgRouting developers
+Mail: project@pgrouting.org
+
+Developer:
+Copyright (c) 2021 Celia Virginia Vergara Castillo
+Copyright (c) 2021 Joseph Emile Honour Percival
+
+------
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+ ********************************************************************PGR-GNU*/
+
+#include <stdbool.h>
+#include "c_common/postgres_connection.h"
+#include <utils/date.h>
+#include "c_common/debug_macro.h"
+#include "c_common/e_report.h"
+#include "c_common/time_msg.h"
+#include "c_common/orders_input.h"
+#include "c_common/get_check_data.h"
+#include "c_common/vehicles_input.h"
+#include "c_common/matrixRows_input.h"
+#include "c_common/time_multipliers_input.h"
+#include "c_types/solution_rt.h"
+#include "c_types/pickDeliveryOrders_t.h"
+#include "drivers/pickDeliver_driver.h"
+
+PGDLLEXPORT Datum _vrp_pickdeliver(PG_FUNCTION_ARGS);
+PG_FUNCTION_INFO_V1(_vrp_pickdeliver);
+PGDLLEXPORT Datum _vrp_pickdeliverraw(PG_FUNCTION_ARGS);
+PG_FUNCTION_INFO_V1(_vrp_pickdeliverraw);
+
+
+static
+void
+process(
+        char* pd_orders_sql,
+        char* vehicles_sql,
+        char* matrix_sql,
+        char* multipliers_sql,
+
+        bool optimize,
+        double factor,
+        int max_cycles,
+        bool stop_on_all_served,
+        int64_t execution_date,
+
+        bool  use_timestamps,
+
+        Solution_rt **result_tuples,
+        size_t *result_count) {
+  /*
+   * Adjusting timestamp data to timezone UTC
+   */
+  if (use_timestamps) {
+    execution_date = timestamp_without_timezone(execution_date);
+  }
+
+  PGR_DBG("execution_date: %ld ", execution_date);
+
+  //! [Factor must be postive]
+  if (factor <= 0) {
+    ereport(ERROR,
+        (errcode(ERRCODE_INTERNAL_ERROR),
+         errmsg("Illegal value in parameter: factor"),
+         errhint("Value found: %f <= 0", factor)));
+  }
+  //! [Factor must be postive]
+
+  if (max_cycles < 0) {
+    ereport(ERROR,
+        (errcode(ERRCODE_INTERNAL_ERROR),
+         errmsg("Illegal value in parameter: max_cycles"),
+         errhint("Value found: %d <= 0", max_cycles)));
+  }
+
+  pgr_SPI_connect();
+
+  PickDeliveryOrders_t *pd_orders_arr = NULL;
+  size_t total_pd_orders = 0;
+  if (use_timestamps) {
+    get_shipments(pd_orders_sql, &pd_orders_arr, &total_pd_orders);
+  } else {
+    get_shipments_raw(pd_orders_sql, &pd_orders_arr, &total_pd_orders);
+  }
+
+  if (total_pd_orders == 0) {
+    (*result_count) = 0;
+    (*result_tuples) = NULL;
+
+    /* freeing memory before return */
+    if (pd_orders_arr) {pfree(pd_orders_arr); pd_orders_arr = NULL;}
+
+    pgr_SPI_finish();
+    return;
+  }
+
+  Vehicle_t *vehicles_arr = NULL;
+  size_t total_vehicles = 0;
+  if (use_timestamps) {
+    get_vehicles(vehicles_sql, &vehicles_arr, &total_vehicles, true);
+  } else {
+    get_vehicles_raw(vehicles_sql, &vehicles_arr, &total_vehicles, true);
+  }
+
+  if (total_vehicles == 0) {
+    (*result_count) = 0;
+    (*result_tuples) = NULL;
+
+    /* freeing memory before return */
+    if (pd_orders_arr) {pfree(pd_orders_arr); pd_orders_arr = NULL;}
+    if (vehicles_arr) {pfree(vehicles_arr); vehicles_arr = NULL;}
+
+    ereport(WARNING,
+        (errcode(ERRCODE_INTERNAL_ERROR),
+         errmsg("No vehicles found")));
+
+    pgr_SPI_finish();
+    return;
+  }
+
+  Time_multipliers_t *multipliers_arr = NULL;
+  size_t total_multipliers_arr = 0;
+  if (use_timestamps) {
+    PGR_DBG("get_timeMultipliers");
+    get_timeMultipliers(multipliers_sql, &multipliers_arr, &total_multipliers_arr);
+  } else {
+    PGR_DBG("get_timeMultipliers_raw");
+    get_timeMultipliers_raw(multipliers_sql, &multipliers_arr, &total_multipliers_arr);
+  }
+
+  if (total_multipliers_arr == 0) {
+    ereport(WARNING,
+        (errcode(ERRCODE_INTERNAL_ERROR),
+         errmsg("No matrix found")));
+    (*result_count) = 0;
+    (*result_tuples) = NULL;
+
+    /* freeing memory before return */
+    if (pd_orders_arr) {pfree(pd_orders_arr); pd_orders_arr = NULL;}
+    if (vehicles_arr) {pfree(vehicles_arr); vehicles_arr = NULL;}
+    if (multipliers_arr) {pfree(multipliers_arr); multipliers_arr = NULL;}
+
+    pgr_SPI_finish();
+    return;
+  }
+
+  Matrix_cell_t *matrix_cells_arr = NULL;
+  size_t total_cells = 0;
+  if (use_timestamps) {
+    get_matrixRows(matrix_sql, &matrix_cells_arr, &total_cells);
+  } else {
+    get_matrixRows_plain(matrix_sql, &matrix_cells_arr, &total_cells);
+  }
+
+  if (total_cells == 0) {
+    (*result_count) = 0;
+    (*result_tuples) = NULL;
+
+    /* freeing memory before return */
+    if (pd_orders_arr) {pfree(pd_orders_arr); pd_orders_arr = NULL;}
+    if (vehicles_arr) {pfree(vehicles_arr); vehicles_arr = NULL;}
+    if (multipliers_arr) {pfree(multipliers_arr); multipliers_arr = NULL;}
+    if (matrix_cells_arr) {pfree(matrix_cells_arr); matrix_cells_arr = NULL;}
+
+    ereport(WARNING,
+        (errcode(ERRCODE_INTERNAL_ERROR),
+         errmsg("No matrix found")));
+    pgr_SPI_finish();
+    return;
+  }
+
+  PGR_DBG("Total %ld orders in query:", total_pd_orders);
+  PGR_DBG("Total %ld vehicles in query:", total_vehicles);
+  PGR_DBG("Total %ld matrix cells in query:", total_cells);
+  PGR_DBG("Total %ld time dependant multipliers:", total_multipliers_arr);
+
+  clock_t start_t = clock();
+  char *log_msg = NULL;
+  char *notice_msg = NULL;
+  char *err_msg = NULL;
+
+  do_pickDeliver(
+      pd_orders_arr,    total_pd_orders,
+      vehicles_arr,     total_vehicles,
+      matrix_cells_arr, total_cells,
+      multipliers_arr,      total_multipliers_arr,
+
+      optimize,
+      factor,
+      max_cycles,
+      stop_on_all_served,
+
+      execution_date,
+
+      result_tuples,
+      result_count,
+
+      &log_msg,
+      &notice_msg,
+      &err_msg);
+
+  time_msg("pgr_pickDeliver", start_t, clock());
+
+  if (err_msg && (*result_tuples)) {
+    pfree(*result_tuples);
+    (*result_count) = 0;
+    (*result_tuples) = NULL;
+  }
+  pgr_global_report(log_msg, notice_msg, err_msg);
+
+  /* freeing memory before return */
+  if (log_msg) {pfree(log_msg); log_msg = NULL;}
+  if (notice_msg) {pfree(notice_msg); notice_msg = NULL;}
+  if (err_msg) {pfree(err_msg); err_msg = NULL;}
+  if (pd_orders_arr) {pfree(pd_orders_arr); pd_orders_arr = NULL;}
+  if (vehicles_arr) {pfree(vehicles_arr); vehicles_arr = NULL;}
+  if (multipliers_arr) {pfree(multipliers_arr); multipliers_arr = NULL;}
+  if (matrix_cells_arr) {pfree(matrix_cells_arr); matrix_cells_arr = NULL;}
+
+  pgr_SPI_finish();
+}
+
+
+
+/**
+ * the timestamp version of the function
+ */
+PGDLLEXPORT Datum
+_vrp_pickdeliver(PG_FUNCTION_ARGS) {
+  FuncCallContext     *funcctx;
+  TupleDesc            tuple_desc;
+
+  /**************************************************************************/
+  Solution_rt *result_tuples = 0;
+  size_t result_count = 0;
+  /**************************************************************************/
+
+  if (SRF_IS_FIRSTCALL()) {
+    MemoryContext   oldcontext;
+    funcctx = SRF_FIRSTCALL_INIT();
+    oldcontext = MemoryContextSwitchTo(funcctx->multi_call_memory_ctx);
+
+    process(
+        text_to_cstring(PG_GETARG_TEXT_P(0)),
+        text_to_cstring(PG_GETARG_TEXT_P(1)),
+        text_to_cstring(PG_GETARG_TEXT_P(2)),
+        text_to_cstring(PG_GETARG_TEXT_P(3)),
+
+        PG_GETARG_BOOL(4),
+        PG_GETARG_FLOAT8(5),
+        PG_GETARG_INT32(6),
+        PG_GETARG_BOOL(7),
+        PG_GETARG_TIMEADT(8),
+        true,
+
+        &result_tuples,
+        &result_count);
+
+    /*********************************************************************/
+
+    funcctx->max_calls = result_count;
+    funcctx->user_fctx = result_tuples;
+    if (get_call_result_type(fcinfo, NULL, &tuple_desc)
+        != TYPEFUNC_COMPOSITE) {
+      ereport(ERROR,
+          (errcode(ERRCODE_FEATURE_NOT_SUPPORTED),
+           errmsg("function returning record called in context "
+             "that cannot accept type record")));
+    }
+
+    funcctx->tuple_desc = tuple_desc;
+    MemoryContextSwitchTo(oldcontext);
+  }
+
+  funcctx = SRF_PERCALL_SETUP();
+  tuple_desc = funcctx->tuple_desc;
+  result_tuples = (Solution_rt*) funcctx->user_fctx;
+
+  if (funcctx->call_cntr <  funcctx->max_calls) {
+    HeapTuple   tuple;
+    Datum       result;
+    Datum       *values;
+    bool*       nulls;
+    size_t      call_cntr = funcctx->call_cntr;
+
+    size_t numb = 16;
+    values = palloc(numb * sizeof(Datum));
+    nulls = palloc(numb * sizeof(bool));
+
+    size_t i;
+    for (i = 0; i < numb; ++i) {
+      nulls[i] = false;
+    }
+
+
+    values[0] = Int32GetDatum(funcctx->call_cntr + 1);
+    values[1] = Int32GetDatum(result_tuples[call_cntr].vehicle_seq);
+    values[2] = Int64GetDatum(result_tuples[call_cntr].vehicle_id);
+    values[3] = Int32GetDatum(result_tuples[call_cntr].stop_seq);
+    values[4] = Int32GetDatum(result_tuples[call_cntr].stop_type + 1);
+    values[5] = Int64GetDatum(result_tuples[call_cntr].stop_id);
+    values[6] = Int64GetDatum(result_tuples[call_cntr].order_id);
+    values[7] = Int64GetDatum(result_tuples[call_cntr].cargo);
+    values[8] = Int64GetDatum(result_tuples[call_cntr].travelTime);
+    values[9] = Int64GetDatum(result_tuples[call_cntr].arrivalTime);
+    values[10] = Int64GetDatum(result_tuples[call_cntr].waitDuration);
+    values[11] = Int64GetDatum(result_tuples[call_cntr].operationTime);
+    values[12] = Int64GetDatum(result_tuples[call_cntr].serviceDuration);
+    values[13] = Int64GetDatum(result_tuples[call_cntr].departureTime);
+    values[14] = Int32GetDatum(result_tuples[call_cntr].cvTot);
+    values[15] = Int32GetDatum(result_tuples[call_cntr].twvTot);
+
+    /*********************************************************************/
+
+    tuple = heap_form_tuple(tuple_desc, values, nulls);
+    result = HeapTupleGetDatum(tuple);
+
+    pfree(values); values = NULL;
+    pfree(nulls); nulls = NULL;
+
+    SRF_RETURN_NEXT(funcctx, result);
+  } else {
+    if (result_tuples) {pfree(result_tuples); result_tuples = NULL;}
+    funcctx->user_fctx = NULL;
+    SRF_RETURN_DONE(funcctx);
+  }
+}
+/******************************************************************************/
+
+
+/**
+ * the plain version of the function
+ */
+PGDLLEXPORT Datum
+_vrp_pickdeliverraw(PG_FUNCTION_ARGS) {
+  FuncCallContext     *funcctx;
+  TupleDesc            tuple_desc;
+
+  /**************************************************************************/
+  Solution_rt *result_tuples = 0;
+  size_t result_count = 0;
+  /**************************************************************************/
+
+  if (SRF_IS_FIRSTCALL()) {
+    MemoryContext   oldcontext;
+    funcctx = SRF_FIRSTCALL_INIT();
+    oldcontext = MemoryContextSwitchTo(funcctx->multi_call_memory_ctx);
+
+    process(
+        text_to_cstring(PG_GETARG_TEXT_P(0)),
+        text_to_cstring(PG_GETARG_TEXT_P(1)),
+        text_to_cstring(PG_GETARG_TEXT_P(2)),
+        text_to_cstring(PG_GETARG_TEXT_P(3)),
+
+        PG_GETARG_BOOL(4),
+        PG_GETARG_FLOAT8(5),
+        PG_GETARG_INT32(6),
+        PG_GETARG_BOOL(7),
+        PG_GETARG_INT64(8),
+        false,
+
+        &result_tuples,
+        &result_count);
+
+    /*********************************************************************/
+
+    funcctx->max_calls = result_count;
+    funcctx->user_fctx = result_tuples;
+    if (get_call_result_type(fcinfo, NULL, &tuple_desc)
+        != TYPEFUNC_COMPOSITE) {
+      ereport(ERROR,
+          (errcode(ERRCODE_FEATURE_NOT_SUPPORTED),
+           errmsg("function returning record called in context "
+             "that cannot accept type record")));
+    }
+
+    funcctx->tuple_desc = tuple_desc;
+    MemoryContextSwitchTo(oldcontext);
+  }
+
+  funcctx = SRF_PERCALL_SETUP();
+  tuple_desc = funcctx->tuple_desc;
+  result_tuples = (Solution_rt*) funcctx->user_fctx;
+
+  if (funcctx->call_cntr <  funcctx->max_calls) {
+    HeapTuple   tuple;
+    Datum       result;
+    Datum       *values;
+    bool*       nulls;
+    size_t      call_cntr = funcctx->call_cntr;
+
+    size_t numb = 16;
+    values = palloc(numb * sizeof(Datum));
+    nulls = palloc(numb * sizeof(bool));
+
+    size_t i;
+    for (i = 0; i < numb; ++i) {
+      nulls[i] = false;
+    }
+
+
+    values[0] = Int32GetDatum(funcctx->call_cntr + 1);
+    values[1] = Int32GetDatum(result_tuples[call_cntr].vehicle_seq);
+    values[2] = Int64GetDatum(result_tuples[call_cntr].vehicle_id);
+    values[3] = Int32GetDatum(result_tuples[call_cntr].stop_seq);
+    values[4] = Int32GetDatum(result_tuples[call_cntr].stop_type + 1);
+    values[5] = Int64GetDatum(result_tuples[call_cntr].stop_id);
+    values[6] = Int64GetDatum(result_tuples[call_cntr].order_id);
+    values[7] = Int64GetDatum(result_tuples[call_cntr].cargo);
+    values[8] = Int64GetDatum(result_tuples[call_cntr].travelTime);
+    values[9] = Int64GetDatum(result_tuples[call_cntr].arrivalTime);
+    values[10] = Int64GetDatum(result_tuples[call_cntr].waitDuration);
+    values[11] = Int64GetDatum(result_tuples[call_cntr].operationTime);
+    values[12] = Int64GetDatum(result_tuples[call_cntr].serviceDuration);
+    values[13] = Int64GetDatum(result_tuples[call_cntr].departureTime);
+    values[14] = Int32GetDatum(result_tuples[call_cntr].cvTot);
+    values[15] = Int32GetDatum(result_tuples[call_cntr].twvTot);
+
+    /*********************************************************************/
+
+    tuple = heap_form_tuple(tuple_desc, values, nulls);
+    result = HeapTupleGetDatum(tuple);
+
+    pfree(values); values = NULL;
+    pfree(nulls); nulls = NULL;
+
+    SRF_RETURN_NEXT(funcctx, result);
+  } else {
+    if (result_tuples) {pfree(result_tuples); result_tuples = NULL;}
+    funcctx->user_fctx = NULL;
+    SRF_RETURN_DONE(funcctx);
+  }
+}
---
+++ src/pickDeliver/pickDeliver_driver.cpp
@@ -0,0 +1,297 @@
+/*PGR-GNU*****************************************************************
+File: pickDeliver_driver.cpp
+
+Copyright (c) 2021 pgRouting developers
+Mail: project@pgrouting.org
+
+Developer:
+Copyright (c) 2021 Celia Virginia Vergara Castillo
+Copyright (c) 2021 Joseph Emile Honour Percival
+
+------
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+ ********************************************************************PGR-GNU*/
+/** @file */
+
+
+#include "drivers/pickDeliver_driver.h"
+
+#include <cstring>
+#include <sstream>
+#include <string>
+#include <deque>
+
+#include "problem/pickDeliver.h"
+#include "c_types/pickDeliveryOrders_t.h"
+#include "c_types/solution_rt.h"
+#include "c_types/vehicle_t.h"
+#include "problem/matrix.h"
+
+#include "cpp_common/pgr_assert.h"
+#include "initialsol/tabu.h"
+#include "optimizers/tabu.h"
+#include "c_common/pgr_alloc.hpp"
+
+/**
+ *
+ *  @param[in] customers_arr A C Array of pickup and dropoff orders
+ *  @param[in] total_customers size of the customers_arr
+ *  @param[in] vehicles_arr A C Array of vehicles
+ *  @param[in] total_vehicles size of the vehicles_arr
+ *  @param[in] matrix_cells_arr A C Array of the (time) matrix cells
+ *  @param[in] total_cells size of the matrix_cells_arr
+ *  @param[in] multipliers_arr A C Array of the multipliers
+ *  @param[in] total_multipliers size of the multipliers_arr
+ *  @param[in] optimize flag to control optimization
+ *  @param[in] factor A global multiplier for the (time) matrix cells
+ *  @param[in] max_cycles number of cycles to perform during the optimization phase
+ *  @param[in] stop_on_all_served Indicator to stop optimization when all orders are served
+ *  @param[in] execution_date Value used for not moving orders that are before this date
+ *  @param[out] return_tuples C array of contents to be returned to postgres
+ *  @param[out] return_count number of tuples returned
+ *  @param[out] log_msg special log message pointer
+ *  @param[out] notice_msg special message pointer to be returned as NOTICE
+ *  @param[out] err_msg special message pointer to be returned as ERROR
+ *  @return void
+ *
+ *
+ * @pre The messages: log_msg, notice_msg, err_msg must be empty (=nullptr)
+ * @pre The C arrays: customers_arr, vehicles_arr, matrix_cells_arr must not be empty
+ * @pre The C array: return_tuples must be empty
+ * @pre Only matrix cells (i, i) can be missing and are considered as 0 (time units)
+ *
+ * @post The C arrays:  customers_arr, vehicles_arr, matrix_cells_arr Do not change
+ * @post The C array: return_tuples contains the result for the problem given
+ * @post The return_tuples array size is return_count
+ * @post err_msg is empty if no throw from the process is catched
+ * @post log_msg contains some logging
+ * @post notice_msg is empty
+ *
+ *
+ @dot
+digraph G {
+  node[fontsize=11, nodesep=0.75,ranksep=0.75];
+
+  start  [shape=Mdiamond];
+  n1  [label="Verify preconditions",shape=rect];
+  n3  [label="Verify matrix cells preconditions",shape=rect];
+  n4  [label="Construct problem",shape=cds,color=blue];
+  n5  [label="get initial solutions",shape=cds,color=blue];
+  n6  [label="solve (optimize)",shape=cds,color=blue];
+  n7  [label="Prepare results",shape=rect];
+  end  [shape=Mdiamond];
+  error [shape=Mdiamond,color=red]
+  start -> n1 -> n3 -> n4 -> n5 -> n6 -> n7 -> end;
+  n1 -> error [ label="Caller error",color=red];
+  n3 -> error [ label="User error",color=red];
+
+}
+@enddot
+
+ *
+ */
+void
+do_pickDeliver(
+    PickDeliveryOrders_t *customers_arr, size_t total_customers,
+    Vehicle_t *vehicles_arr, size_t total_vehicles,
+    Matrix_cell_t *matrix_cells_arr, size_t total_cells,
+    Time_multipliers_t *multipliers_arr, size_t total_multipliers,
+
+
+    bool optimize,
+    double factor,
+    int max_cycles,
+    bool stop_on_all_served,
+    int64_t execution_date,
+
+    Solution_rt **return_tuples,
+    size_t *return_count,
+
+    char **log_msg,
+    char **notice_msg,
+    char **err_msg) {
+  std::ostringstream log;
+  std::ostringstream notice;
+  std::ostringstream err;
+  try {
+    /*
+     * verify preconditions
+     */
+    pgassert(!(*log_msg));
+    pgassert(!(*notice_msg));
+    pgassert(!(*err_msg));
+    pgassert(total_customers);
+    pgassert(total_vehicles);
+    pgassert(total_cells);
+    pgassert(*return_count == 0);
+    pgassert(!(*return_tuples));
+    log << "do_pickDeliver\n";
+
+    *return_tuples = nullptr;
+    *return_count = 0;
+
+    Identifiers<Id> node_ids;
+    Identifiers<Id> order_ids;
+
+    for (size_t i = 0; i < total_customers; ++i) {
+      node_ids += customers_arr[i].pick_node_id;
+      node_ids += customers_arr[i].deliver_node_id;
+      order_ids += customers_arr[i].id;
+    }
+
+    bool missing = false;
+    for (size_t i = 0; i < total_vehicles; ++i) {
+      auto vehicle = vehicles_arr[i];
+      node_ids += vehicle.start_node_id;
+      node_ids += vehicle.end_node_id;
+      for (size_t j = 0; j < vehicle.stops_size; ++j) {
+        if (!order_ids.has(vehicle.stops[j])) {
+          if (!missing) err << "Order in 'stops' information missing";
+          missing = true;
+          err << "Missing information of order " << vehicle.stops[j] << "\n";
+        }
+      }
+      if (missing) {
+        *err_msg = pgr_msg(err.str());
+        return;
+      }
+    }
+
+    vrprouting::problem::Matrix cost_matrix(
+        matrix_cells_arr, total_cells,
+        multipliers_arr, total_multipliers,
+        node_ids, static_cast<Multiplier>(factor));
+#if 1
+    /*
+     * Verify matrix triangle inequality
+     */
+    if (!cost_matrix.obeys_triangle_inequality()) {
+      log << "[PickDeliver] Fixing Matrix that does not obey triangle inequality ";
+      log << cost_matrix.fix_triangle_inequality() << " cycles used";
+
+      if (!cost_matrix.obeys_triangle_inequality()) {
+        log << "[pickDeliver] Matrix Still does not obey triangle inequality ";
+      }
+    }
+#endif
+    /*
+     * Verify matrix cells preconditions
+     */
+    if (!cost_matrix.has_no_infinity()) {
+      err << "An Infinity value was found on the Matrix";
+      *err_msg = pgr_msg(err.str());
+      return;
+    }
+
+
+    log << "stop_on_all_served" << stop_on_all_served << "\n";
+    log << "execution_date" << execution_date << "\n";
+    log << "Initialize problem\n";
+    /*
+     * Construct problem
+     */
+    vrprouting::problem::PickDeliver pd_problem(
+        customers_arr, total_customers,
+        vehicles_arr, total_vehicles,
+        cost_matrix);
+
+    err << pd_problem.msg.get_error();
+    if (!err.str().empty()) {
+      log << pd_problem.msg.get_error();
+      log << pd_problem.msg.get_log();
+      *log_msg = pgr_msg(log.str());
+      *err_msg = pgr_msg(err.str());
+      return;
+    }
+    log << pd_problem.msg.get_log();
+    pd_problem.msg.clear();
+
+    log << "Finish Initialize problem\n";
+
+#if 0
+    try {
+#endif
+      /*
+       * get initial solutions
+       */
+      using Initial_solution = vrprouting::initialsol::tabu::Initial_solution;
+      using Solution = vrprouting::problem::Solution;
+      auto sol = static_cast<Solution>(Initial_solution(execution_date, optimize, &pd_problem));
+      /*
+       * Solve (optimize)
+       */
+      using Optimize = vrprouting::optimizers::tabu::Optimize;
+      sol = Optimize(sol, static_cast<size_t>(max_cycles), stop_on_all_served, optimize);
+#if 0
+    } catch (AssertFailedException &except) {
+      log << pd_problem.msg.get_log();
+      throw;
+    } catch(...) {
+      log << "Caught unknown exception!";
+      throw;
+    }
+#endif
+
+    log << pd_problem.msg.get_log();
+    pd_problem.msg.clear();
+    log << "Finish solve\n";
+
+    /*
+     * Prepare results
+     */
+    auto solution = sol.get_postgres_result();
+    log << pd_problem.msg.get_log();
+    pd_problem.msg.clear();
+    log << "solution size: " << solution.size() << "\n";
+
+
+    if (!solution.empty()) {
+      (*return_tuples) = pgr_alloc(solution.size(), (*return_tuples));
+      int seq = 0;
+      for (const auto &row : solution) {
+        (*return_tuples)[seq] = row;
+        ++seq;
+      }
+    }
+    (*return_count) = solution.size();
+
+    log << pd_problem.msg.get_log();
+
+    pgassert(*err_msg == nullptr);
+    *log_msg = log.str().empty()?
+      nullptr :
+      pgr_msg(log.str());
+    *notice_msg = notice.str().empty()?
+      nullptr :
+      pgr_msg(notice.str());
+  } catch (AssertFailedException &except) {
+    if (*return_tuples) free(*return_tuples);
+    (*return_count) = 0;
+    err << except.what() << log.str();
+    *err_msg = pgr_msg(err.str());
+  } catch (std::exception& except) {
+    if (*return_tuples) free(*return_tuples);
+    (*return_count) = 0;
+    err << except.what() << log.str();
+    *err_msg = pgr_msg(err.str());
+  } catch(...) {
+    if (*return_tuples) free(*return_tuples);
+    (*return_count) = 0;
+    err << "Caught unknown exception!" << log.str();
+    *err_msg = pgr_msg(err.str());
+  }
+}
