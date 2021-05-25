aa5482791772e3de21ad0561d66c34088225d1f9
Iosefa Percival <ipercival@gmail.com>
2021-05-25 09:02:52 +0900

    [docqueries] Adding pseudo-tests for vrp_optimize

---
+++ docqueries/optimize/optimize/09-12-mid-day-partial-false.test.sql
@@ -0,0 +1,39 @@
+SET search_path TO 'example2', 'public';
+
+DROP TABLE IF EXISTS tmp_vehicles;
+SELECT *
+INTO tmp_vehicles
+FROM vehicles;
+
+COMMIT;
+
+SET client_min_messages TO NOTICE;
+
+CALL vrp_optimize(
+    $$SELECT * FROM shipments$$,
+    $$SELECT * FROM tmp_vehicles$$,
+    $$SELECT start_vid, end_vid, travel_time FROM timeMatrix$$,
+    $$SELECT * FROM tdm('2019-12-09'::TIMESTAMP)$$,
+    'tmp_vehicles',
+    -- optional parameters
+    execution_date => '2019-12-09 01:00:00',
+    factor => 1::FLOAT,
+    max_cycles => 1
+);
+
+BEGIN;
+
+/** no changes */
+SELECT count(*) = 36
+FROM vehicles v
+LEFT JOIN tmp_vehicles t
+USING (s_tw_open, id)
+WHERE date_trunc('day', s_tw_open) <= '2019-12-09 00:00:00';
+
+/** changes */
+SELECT id, s_tw_open, v.stops, t.stops
+FROM vehicles v
+LEFT JOIN tmp_vehicles t
+USING (s_tw_open, id)
+WHERE date_trunc('day', s_tw_open) > '2019-12-09 00:00:00'
+ORDER BY s_tw_open, id;
---
+++ docqueries/optimize/optimize/09-12-partial-false.test.sql
@@ -0,0 +1,28 @@
+SET search_path TO 'example2', 'public';
+
+DROP TABLE IF EXISTS tmp_vehicles;
+SELECT *
+INTO tmp_vehicles
+FROM vehicles;
+
+COMMIT;
+
+SET client_min_messages TO NOTICE;
+
+CALL vrp_optimize(
+    $$SELECT * FROM shipments$$,
+    $$SELECT * FROM tmp_vehicles$$,
+    $$SELECT start_vid, end_vid, travel_time FROM timeMatrix$$,
+    $$SELECT * FROM tdm('2019-12-09'::TIMESTAMP)$$,
+    'tmp_vehicles',
+    '2019-12-09 00:00:00',  factor => 1::FLOAT, max_cycles => 1
+);
+
+BEGIN;
+
+/** changes */
+SELECT id, s_tw_open, v.stops, t.stops
+FROM vehicles v
+LEFT JOIN tmp_vehicles t
+USING (s_tw_open, id)
+ORDER BY s_tw_open, id;
