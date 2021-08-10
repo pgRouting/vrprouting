11183e15618842efab9bda6e99308f056d9178e5
Ashish Kumar <ashishkr23438@gmail.com>
2021-08-10 19:34:40 +0530

    [vroom][docqueries] Added vroom function docqueries

---
+++ docqueries/vroom/doc-vrp_vroomShipments.test.sql
@@ -0,0 +1,36 @@
+\echo -- q1
+SELECT *
+FROM vrp_vroomShipments(
+  'SELECT * FROM vroom.shipments',
+  'SELECT * FROM vroom.shipments_time_windows',
+  'SELECT * FROM vroom.vehicles',
+  'SELECT * FROM vroom.breaks',
+  'SELECT * FROM vroom.breaks_time_windows',
+  'SELECT * FROM vroom.matrix'
+);
+\echo -- q2
+SELECT *
+FROM vrp_vroomShipments(
+  $shipments$
+    SELECT * FROM (
+      VALUES (100, 1, 4)
+    ) AS C(id, p_location_index, d_location_index)
+  $shipments$,
+  NULL,
+  $vehicles$
+    SELECT * FROM (
+      VALUES (1, 1, 4)
+    ) AS C(id, start_index, end_index)
+  $vehicles$,
+  NULL,
+  NULL,
+  $matrix$
+    SELECT * FROM (
+      VALUES (1, 2, 2104), (1, 3, 197), (1, 4, 1299),
+             (2, 1, 2103), (2, 3, 2255), (2, 4, 3152),
+             (3, 1, 197), (3, 2, 2256), (3, 4, 1102),
+             (4, 1, 1299), (4, 2, 3153), (4, 3, 1102)
+    ) AS C(start_vid, end_vid, agg_cost)
+  $matrix$
+);
+\echo -- q3
