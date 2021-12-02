d5e5b08e628967c1b8b3239d9092e1224f2f7b59
Ashish Kumar <ashishkr23438@gmail.com>
2021-12-02 10:20:44 +0530

    [vroom][docquery] Update docqueries for custom cost matrices

--- docqueries/vroom/doc-vrp_vroomJobs.test.sql
+++ docqueries/vroom/doc-vrp_vroomJobs.test.sql
@@ -11,6 +11,7 @@ ALTER TABLE vroom.shipments_time_windows ALTER COLUMN tw_open TYPE TIMESTAMP USI
 ALTER TABLE vroom.shipments_time_windows ALTER COLUMN tw_close TYPE TIMESTAMP USING (to_timestamp(tw_close + 1630573200) at time zone 'UTC')::TIMESTAMP;
 ALTER TABLE vroom.breaks_time_windows ALTER COLUMN tw_open TYPE TIMESTAMP USING (to_timestamp(tw_open + 1630573200) at time zone 'UTC')::TIMESTAMP;
 ALTER TABLE vroom.breaks_time_windows ALTER COLUMN tw_close TYPE TIMESTAMP USING (to_timestamp(tw_close + 1630573200) at time zone 'UTC')::TIMESTAMP;
+ALTER TABLE vroom.matrix ALTER COLUMN duration TYPE INTERVAL USING make_interval(secs => duration);
 /* -- q1 */
 SELECT *
 FROM vrp_vroomJobs(
@@ -38,12 +39,12 @@ FROM vrp_vroomJobs(
   NULL,
   NULL,
   $matrix$
-    SELECT * FROM (
+    SELECT start_id, end_id, make_interval(secs => duration) AS duration FROM (
       VALUES (1, 2, 2104), (1, 3, 197), (1, 4, 1299),
              (2, 1, 2103), (2, 3, 2255), (2, 4, 3152),
              (3, 1, 197), (3, 2, 2256), (3, 4, 1102),
              (4, 1, 1299), (4, 2, 3153), (4, 3, 1102)
-    ) AS C(start_vid, end_vid, agg_cost)
+    ) AS C(start_id, end_id, duration)
   $matrix$
 );
 /* -- q3 */
