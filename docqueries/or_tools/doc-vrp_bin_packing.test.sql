b42f7bfc73248725898cb4452b8b421c173d44d8
cvvergara <vicky@erosion.dev>
2024-07-19 14:05:41 -0600

    (docqueries/or-tools) the data is no longer on the test

--- docqueries/or_tools/doc-vrp_bin_packing.test.sql
+++ docqueries/or_tools/doc-vrp_bin_packing.test.sql
@@ -1,17 +1,4 @@
-DROP TABLE IF EXISTS bin_packing_query CASCADE;
-
-CREATE TABLE bin_packing_query(
-  id INTEGER,
-  weight INTEGER);
-
-INSERT INTO bin_packing_query (id, weight)
-VALUES
-(1, 48), (2, 30), (3, 19), (4, 36), (5, 36), (6, 27), (7, 42), (8, 42), (9, 36), (10, 24), (11, 30);
-
 /* -- example_start */
 SELECT *
-FROM bin_packing_query;
-
-SELECT * 
-FROM vrp_bin_packing('SELECT id, weight FROM bin_packing_query', 100);
+FROM vrp_bin_packing($$SELECT id, weight FROM ortools.bin_packing$$, 100);
 /* -- example_end */
