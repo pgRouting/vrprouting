2b34a6b0ef2bae0b51d07791a67f3aa685e74d7e
cvvergara <vicky@erosion.dev>
2024-07-19 14:08:08 -0600

    (docqueries/simulation) changing the comment style

--- docqueries/simulation/full_simulation.test.sql
+++ docqueries/simulation/full_simulation.test.sql
@@ -8,14 +8,14 @@ CALL vrp_simulation(
     $$SELECT start_vid, end_vid, travel_time FROM timeMatrix$$,
     $$SELECT * FROM tdm('2019-12-09'::TIMESTAMP, '2019-12-13'::TIMESTAMP)$$,
 
-    1.0, -- factor
-    1, -- max_cycles when adding an order
-    10, -- max_cycles when response time is big or day changes
+    1.0, /* factor */
+    1, /* max_cycles when adding an order */
+    10, /* max_cycles when response time is big or day changes */
 
-    '2019-12-02 00:00:00'::TIMESTAMP, -- start simulation from this "booking date"
+    '2019-12-02 00:00:00'::TIMESTAMP, /* start simulation from this "booking date" */
 
-    print => 1000, -- print vehicle stops every 1000 bookings
-    optimize => true -- turn optimization on
+    print => 1000, /* print vehicle stops every 1000 bookings */
+    optimize => true /* turn optimization on */
 );
 
 BEGIN;
