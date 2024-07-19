c9281d08f873954b451ae5cf76c22f1af131af05
cvvergara <vicky@erosion.dev>
2024-07-19 13:45:44 -0600

    (tools/testers/pg_prove_tests.sh) Adjusting test scripts to data changes

--- tools/testers/pg_prove_tests.sh
+++ tools/testers/pg_prove_tests.sh
@@ -8,25 +8,59 @@
 
 set -e
 
-PGUSER=$1
-PGPORT=$2
-if [ "b$PGPORT" = "b" ]
-then
-    PGPORT="-p 5432"
-else
-    PGPORT="-p $PGPORT"
-fi
 PGDATABASE="___vrp___test___"
-echo "$PGPORT"
 
-dropdb --if-exists ___vrp___test___
-createdb ___vrp___test___
+while [[ $# -gt 0 ]]; do
+  case $1 in
+    -U)
+      PGUSER=(-U "$2")
+      shift
+      shift
+      ;;
+    -d)
+      PGDATABASE="$2"
+      shift
+      shift
+      ;;
+    -c|--clean)
+      CLEANDB=YES
+      shift
+      ;;
+    -p)
+      PGPORT=(-p "$2")
+      shift
+      shift
+      ;;
+    -*)
+      echo "Unknown option $1"
+      exit 1
+      ;;
+    *)
+        POSITIONAL_ARGS+=("$1") # save positional arg
+        shift # past argument
+        ;;
+  esac
+done
 
-echo "cd ./tools/testers/"
+set -- "${POSITIONAL_ARGS[@]}" # restore positional parameters
+
+echo PGUSER= "${PGUSER[@]}"
+echo PGPORT= "${PGPORT[@]}"
+echo PGDATABASE= "${PGDATABASE}"
+echo CLEANDB= "${CLEANDB}"
+
+if [ -n "${CLEANDB}" ]; then
+    echo "Recreating database ${PGDATABASE}"
+    dropdb --if-exists "${PGPORT[@]}" "${PGUSER[@]}" "${PGDATABASE}"
+    createdb "${PGPORT[@]}" "${PGUSER[@]}" "${PGDATABASE}"
+fi
+
 cd ./tools/testers/
-echo "psql -f setup_db.sql"
-psql "$PGPORT" -U "$PGUSER"  -d "$PGDATABASE" -X -q -v ON_ERROR_STOP=1 --pset pager=off -f setup_db.sql
+psql "${PGPORT[@]}" "${PGUSER[@]}" -d "${PGDATABASE}" -X -q -v ON_ERROR_STOP=1 --pset pager=off -f setup_db.sql
 
-pg_prove --failures --quiet --recurse --ext .sql "$PGPORT" -d "$PGDATABASE"  -U "$PGUSER"  ../../pgtap/
+pg_prove --failures --quiet --recurse "${PGPORT[@]}" "${PGUSER[@]}" -d "${PGDATABASE}" ../../pgtap/
 
-dropdb --if-exists ___vrp___test___
+# database wont be removed unless script does not fails
+if [ -n "$CLEANDB" ]; then
+    dropdb --if-exists "${PGPORT[@]}" "${PGUSER[@]}" "${PGDATABASE}"
+fi
