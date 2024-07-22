19474e77faf5be4ee4eca6c07e6fa48f613597c0
cvvergara <vicky@erosion.dev>
2024-07-22 10:23:45 -0600

    (version) Adjusting docqueries & pgtap

--- pgtap/version/full_version.sql
+++ pgtap/version/full_version.sql
@@ -11,13 +11,13 @@ SELECT function_returns('vrp_full_version', ARRAY[]::text[], 'record');
 
 SELECT set_eq(
     $$SELECT  proargnames from pg_proc where proname = 'vrp_full_version'$$,
-    $$SELECT  '{"version","build_type","compile_date","library","system","postgresql","compiler","boost","hash"}'::TEXT[] $$
+    $$SELECT  '{"version","build_type","compile_date","library","system","postgresql","compiler","hash"}'::TEXT[] $$
 );
 
 -- parameter types
 SELECT set_eq(
     $$SELECT  proallargtypes from pg_proc where proname = 'vrp_full_version'$$,
-    $$SELECT  '{25,25,25,25,25,25,25,25,25}'::OID[] $$
+    $$SELECT  '{25,25,25,25,25,25,25,25}'::OID[] $$
 );
 
 
