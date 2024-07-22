4b21dd055e7ba74cf5404ffed97ac0640666a384
cvvergara <vicky@erosion.dev>
2024-07-22 13:15:18 -0600

    (OR Tools)(sql) fix for or-tools 9.10.4067 and cleanup

--- sql/or_tools/bin_packing.sql
+++ sql/or_tools/bin_packing.sql
@@ -24,106 +24,20 @@ along with this program; if not, write to the Free Software
 Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
  ********************************************************************PGR-GNU*/
-/*
-signature start
 
-.. code-block:: none
-
-    vrp_bin_packing(
-      Weights SQL, bin_capacity ANY-INTEGER, [, max_rows])
-
-    RETURNS SET OF
-    (bin_number, item_id)
-
-signature end
-
-parameters start
-
-============================== ================ ============================================================
-Parameter                      Type             Description
-============================== ================ ============================================================
-**Weights SQL**                ``TEXT``         `Weights SQL`_ query describing the weight of each item
-**Bin_Capacity**               ``ANY-INTEGER``  Maximum Capacity of the bin.
-============================== ================ ============================================================
-
-parameters end
-
-optional parameters start
-
-===================== =============== ================================ ==================================================
-Parameter             Type            Default                          Description
-===================== =============== ================================ ==================================================
-**max_rows**          ``ANY-INTEGER`` :math:`100000`                   Maximum items(rows) to fetch from bin_packing_data
-                                                                       table
-===================== =============== ================================ ==================================================
-
-optional parameters end
-
-.. Weights start
-
-A ``SELECT`` statement that returns the following columns:
-
-::
-
-    id, weight
-
-
-====================  =========================  =========== ================================================
-Column                Type                       Default     Description
-====================  =========================  =========== ================================================
-**id**                ``ANY-INTEGER``                        unique identifier of the item.
-
-**weight**            ``ANY-INTEGER``                        weight of the item.
-====================  =========================  =========== ================================================
-
-.. Weights end
-
-result start
-
-Returns set of
-
-.. code-block:: none
-
-    (bin_number, item_id)
-
-=================== ================= =================================================
-Column              Type              Description
-=================== ================= =================================================
-**bin_number**       ``ANY-INTEGER``       Integer to uniquely identify a bin
-
-**item_id**          ``ANY-INTEGER``       Integer to uniquely identify an item in the 
-                                           bin
-=================== ================= =================================================
-result end
-
-**Note**:
-
-- ANY-INTEGER: [SMALLINT, INTEGER, BIGINT]
-*/
-DROP FUNCTION IF EXISTS vrp_bin_packing CASCADE;
--- DROP TABLE IF EXISTS bin_packing_data CASCADE;
-
--- CREATE TABLE bin_packing_data(
---   id INTEGER,
---   weight INTEGER);
-
--- INSERT INTO bin_packing_data (id, weight)
--- VALUES
--- (1, 48), (2, 30), (3, 19), (4, 36), (5, 36), (6, 27), (7, 42), (8, 42), (9, 36), (10, 24), (11, 30);
-
-
+-- v0.4
 CREATE OR REPLACE FUNCTION vrp_bin_packing(
   inner_query TEXT, -- weights SQL
   bin_capacity INTEGER, -- Bin Capacity
   max_rows INTEGER = 100000 -- Maximum number of rows to be fetched. Default is value = 100000.
 )
-RETURNS TABLE(bin_number INTEGER, item_id INTEGER)
+RETURNS TABLE(bin INTEGER, id INTEGER)
 AS $$
   try:
     from ortools.linear_solver import pywraplp
   except Exception as err:
     plpy.error(err)
-  
+
   global max_rows
   if inner_query == None:
     raise Exception('Inner Query Cannot be NULL')
@@ -131,7 +45,7 @@ AS $$
     raise Exception('Capacity Cannot be NULL')
   if max_rows == None:
     max_rows = 100000
-  
+
   # Program Execution Starts here
 
   try:
@@ -154,7 +68,7 @@ AS $$
     pass
   else:
     raise Exception("Returned columns of different type. Expected Integer")
-  
+
   data = {}
   weights = []
   data_ids = []
@@ -162,21 +76,21 @@ AS $$
   for i in range(num_of_rows):
     weights.append(inner_query_result[i]["weight"])
     data_ids.append(inner_query_result[i]["id"])
-      
+
 
   data['weights'] = weights
   data['items'] = list(range(len(weights)))
   data['bins'] = data['items']
   data['bin_capacity'] = bin_capacity
-  
+
   try:
-    solver = pywraplp.Solver.CreateSolver('SCIP')  
+    solver = pywraplp.Solver.CreateSolver('SCIP')
   except:
     plpy.error("Unable to Initialize solver")
-  
+
   if solver is None:
     plpy.error('SCIP solver unavailable.')
-    
+
 
   x = {}
   for i in data['items']:
@@ -191,7 +105,7 @@ AS $$
     solver.Add(sum(x[i, j] for j in data['bins']) == 1)
 
   for j in data['bins']:
-    solver.Add(sum(x[(i, j)] * data['weights'][i] 
+    solver.Add(sum(x[(i, j)] * data['weights'][i]
     for i in data['items']) <= y[j] * data['bin_capacity'])
 
   solver.Minimize(solver.Sum([y[j] for j in data['bins']]))
@@ -216,10 +130,6 @@ AS $$
   # end of the program
 
 $$ LANGUAGE plpython3u VOLATILE;
-
--- SELECT * FROM vrp_bin_packing('SELECT id, weight FROM bin_packing_data', 100);
-
--- COMMENTS
 
 COMMENT ON FUNCTION vrp_bin_packing(TEXT, INTEGER, INTEGER)
 IS 'vrp_bin_packing
--- sql/or_tools/knapsack.sql
+++ sql/or_tools/knapsack.sql
@@ -26,112 +26,19 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
  ********************************************************************PGR-GNU*/
 
-/*
-signature start
-
-.. code-block:: none
-
-    vrp_knapsack(
-      Weights_Costs SQL, capacity ANY-INTEGER, [, max_rows])
-
-    RETURNS SET OF
-    (item_id)
-
-signature end
-
-parameters start
-
-============================== ================ =========================================================
-Parameter                      Type             Description
-============================== ================ =========================================================
-**Weights_Costs SQL**          ``TEXT``         `Weights_Costs SQL`_ query describing the weights and
-                                                cost of each item
-**Capacity**                   ``ANY-INTEGER``  Maximum Capacity of the knapsack.
-============================== ================ =========================================================
-
-parameters end
-
-optional parameters start
-
-===================== =============== ================================== =================================================
-Parameter             Type            Default                            Description
-===================== =============== ================================== =================================================
-**max_rows**          ``ANY-INTEGER`` :math:`100000`                     Maximum items(rows) to fetch from knapsack_data
-                                                                         table
-===================== =============== ================================== =================================================
-
-optional parameters end
-
-.. Weights_Costs start
-
-A ``SELECT`` statement that returns the following columns:
-
-::
-
-    id, weight, cost
-
-
-====================  =========================  =========== ================================================
-Column                Type                       Default     Description
-====================  =========================  =========== ================================================
-**id**                ``ANY-INTEGER``                        unique identifier of the item.
-
-**weight**            ``ANY-INTEGER``                        weight of the item.
-
-**cost**              ``ANY-INTEGER``                        cost of the item.
-====================  =========================  =========== ================================================
-
-.. Weights_Costs end
-
-result start
-
-Returns set of
-
-.. code-block:: none
-
-    (item_id)
-
-=================== ================= =================================================
-Column              Type              Description
-=================== ================= =================================================
-**item_id**         ``ANY-INTEGER``   Integer to uniquely identify an item in the 
-                                      knapsack
-=================== ================= =================================================
-result end
-
-**Note**:
-
-- ANY-INTEGER: [SMALLINT, INTEGER, BIGINT]
-*/
-
-DROP FUNCTION IF EXISTS vrp_knapsack CASCADE;
--- DROP TABLE IF EXISTS knapsack_data;
-
--- CREATE TABLE knapsack_data(
---   id INTEGER,
---   weight INTEGER,
---   cost INTEGER);
-
--- INSERT INTO knapsack_data (id, weight,  cost)
--- VALUES
--- (1, 12, 4),
--- (2, 2, 2),
--- (3, 1, 1),
--- (4, 4, 10),
--- (5, 1, 2);
-
+-- v0.4
 CREATE OR REPLACE FUNCTION vrp_knapsack(
   inner_query TEXT, -- weights_cost SQL
   capacity INTEGER, -- Knapsack Capacity
   max_rows INTEGER = 100000 -- Maximum number of rows to be fetched. Default is 100000.
 )
-RETURNS TABLE(item_id INTEGER)
+RETURNS TABLE(id INTEGER)
 AS $$
   try:
-    from ortools.algorithms import pywrapknapsack_solver
+    from ortools.algorithms.python import knapsack_solver
   except Exception as err:
     plpy.error(err)
-  
+
   global max_rows
   if inner_query == None:
     raise Exception('Inner Query Cannot be NULL')
@@ -140,12 +47,12 @@ AS $$
   if max_rows == None:
     max_rows = 100000
   try:
-    solver = pywrapknapsack_solver.KnapsackSolver(
-    pywrapknapsack_solver.KnapsackSolver.
+    solver = knapsack_solver.KnapsackSolver(
+    knapsack_solver.SolverType.
     KNAPSACK_MULTIDIMENSION_BRANCH_AND_BOUND_SOLVER, 'KnapsackExample')
   except:
     plpy.error('Unable to Initialize Knapsack Solver')
-  
+
   capacities = []
   capacities.append(capacity)
 
@@ -158,14 +65,14 @@ AS $$
     coltypes = inner_query_result.coltypes()
   except plpy.SPIError as error_msg:
     plpy.error("Error Processing Inner Query. The given query is not a valid SQL command")
-  
+
   if len(colnames) != 3:
     plpy.error("Expected 3 columns, Got ", len(colnames))
   if ('weight' in colnames) and ('cost' in colnames) and ('id' in colnames):
     # got correct column names
     pass
   else:
-    plpy.error("Expected columns weight and cost, Got ", colnames) 
+    plpy.error("Expected columns weight and cost, Got ", colnames)
   if all(item in [20, 21, 23] for item in coltypes):
     # got correct column types
     pass
@@ -181,25 +88,21 @@ AS $$
     values.append(inner_query_result[i]["cost"])
     weight1.append(inner_query_result[i]["weight"])
   weights.append(weight1)
-  
+
   try:
-    solver.Init(values, weights, capacities)
+    solver.init(values, weights, capacities)
   except Exception as error_msg:
     plpy.error(error_msg)
-  computed_value = solver.Solve()
-  
+  computed_value = solver.solve()
+
   # prints results
   for i in range(len(values)):
-    if solver.BestSolutionContains(i):
+    if solver.best_solution_contains(i):
       yield (ids[i])
 
   # end of the program
 
 $$ LANGUAGE plpython3u VOLATILE;
-
--- SELECT * FROM vrp_knapsack('SELECT id, weight, cost FROM knapsack_data' , 15);
-
--- COMMENTS
 
 COMMENT ON FUNCTION vrp_knapsack(TEXT, INTEGER, INTEGER)
 IS 'vrp_knapsack
