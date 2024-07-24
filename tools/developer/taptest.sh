c7a46b3c0fa468037dd2872d1a655692d8ca4c46
cvvergara <vicky@erosion.dev>
2024-07-24 08:26:08 -0600

    adjusting pgtap tests to extension .pg

--- tools/developer/taptest.sh
+++ tools/developer/taptest.sh
@@ -2,27 +2,35 @@
 
 set -e
 
+QUIET="-v"
+QUIET="-q"
+
 if [[ -z  $1 ]]; then
-    echo "Directory missing";
-    echo "Usage"
-    echo "tools/developer/taptest.sh <pgtap directory> [<postgresql options>]";
+    echo "taptest.sh help:"
+    echo Copy on the repository root directory and adjust to your needs
+    echo "Usage:"
+    echo "taptest.sh <pgtap file or directory> [<postgresql options>]";
     echo "Examples:"
-    echo "tools/developer/taptest.sh withPoints/undirected_equalityDD.sql -p 5433"
+    echo "taptest.sh pgtap/withPoints/directed_equalityDD.pg -v -p 5433"
+    echo "taptest.sh pgtap/withPoints -p 5433"
     exit 1;
 fi
 
 DIR="$1"
 shift
-PGFLAGS=$*
+PGFLAGS="$*"
 
-PGDATABASE="___vrp___test___"
+echo "----------------------------------------------------"
+echo "-----------  taptest of:  $DIR"
+echo "----------------------------------------------------"
+PGDATABASE="___vrp___taptest___"
 
-dropdb --if-exists "${PGFLAGS}" "${PGDATABASE}"
-createdb "${PGFLAGS}" "${PGDATABASE}"
+dropdb  ${PGFLAGS} --if-exists "${PGDATABASE}"
+createdb ${PGFLAGS} "${PGDATABASE}"
 
-echo "../../${DIR}"
-
-cd tools/testers/
-psql "$PGFLAGS"  -f setup_db.sql -d "${PGDATABASE}"
-pg_prove -v --recurse --ext .sql "${PGFLAGS}"  -d "${PGDATABASE}" "../../${DIR}"
-dropdb --if-exists "${PGFLAGS}" "${PGDATABASE}"
+pushd tools/testers/
+psql "${PGFLAGS}"  -f setup_db.sql -d "${PGDATABASE}"
+popd
+pg_prove --recurse ${QUIET} ${PGFLAGS}  -d "${PGDATABASE}" "${DIR}"
+echo pg_prove --recurse ${QUIET} ${PGFLAGS}  -d "${PGDATABASE}" "${DIR}"
+#dropdb --if-exists "${PGFLAGS}" "${PGDATABASE}"
