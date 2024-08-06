4c166599ebbfd8c0039639ad21d664bc3d25cfe0
cvvergara <vicky@erosion.dev>
2024-08-06 13:21:33 -0600

    *.c Not supporting postgres 9x

--- src/optimize/optimize.c
+++ src/optimize/optimize.c
@@ -288,11 +288,7 @@ _vrp_optimize(PG_FUNCTION_ARGS) {
         &result_tuples,
         &result_count);
 
-#if PGSQL_VERSION > 95
     funcctx->max_calls = result_count;
-#else
-    funcctx->max_calls = (uint32_t)result_count;
-#endif
     funcctx->user_fctx = result_tuples;
     if (get_call_result_type(fcinfo, NULL, &tuple_desc)
         != TYPEFUNC_COMPOSITE) {
