5d9bde687d79150f52e2b3c7de6de960a27bed97
cvvergara <vicky@erosion.dev>
2025-08-15 13:35:48 -0600

    (data) Not using pgrouting, vroom ortools on sample data

--- tools/testers/setup_db.sql
+++ tools/testers/setup_db.sql
@@ -15,9 +15,8 @@ SET client_min_messages = WARNING;
 
 CREATE EXTENSION IF NOT EXISTS pgtap;
 CREATE EXTENSION IF NOT EXISTS vrprouting CASCADE;
-CREATE EXTENSION IF NOT EXISTS postgis;
-CREATE EXTENSION IF NOT EXISTS pgrouting;
-CREATE EXTENSION IF NOT EXISTS plpython3u;
+CREATE EXTENSION IF NOT EXISTS postgis CASCADE;
+CREATE EXTENSION IF NOT EXISTS pgrouting CASCADE;
 
 BEGIN;
 
@@ -27,9 +26,6 @@ BEGIN;
     \i solomon_100_rc101.data.sql
     \i no_crash_test.sql
     \i general_pgtap_tests.sql
-    \i vroomdata.sql
-    \i ortoolsdata.sql
     \i functions.sql
-    \i solomon_100_rc101.data.sql
 
 END;
